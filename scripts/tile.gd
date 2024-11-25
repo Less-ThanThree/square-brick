e31f95f0468b9b9af0be5766c5852f8c442b9d4a
EugenyX_X <38291801+PistrunXvX@users.noreply.github.com>
2024-11-26 02:50:47 +0300

    Add tile tweens and meeples grid
    Добавил твины движения и сетку для установки миплов

--- scripts/tile.gd
+++ scripts/tile.gd
@@ -3,10 +3,21 @@ extends Control
 @export var tile_info: Dictionary
 
 @onready var tile_sprite = $Tile_img
+@onready var meeple_grid = $MeepleGrid
+@onready var panel_1 = $MeepleGrid/Panel
+@onready var panel_2 = $MeepleGrid/Panel2
+@onready var panel_3 = $MeepleGrid/Panel3
+@onready var panel_4 = $MeepleGrid/Panel4
+@onready var panel_5 = $MeepleGrid/Panel5
+@onready var panel_6 = $MeepleGrid/Panel6
+@onready var panel_7 = $MeepleGrid/Panel7
+@onready var panel_8 = $MeepleGrid/Panel8
+@onready var panel_9 = $MeepleGrid/Panel9
 
 var matrix_top_level: Array
 var matrix_down_level: Dictionary
 var angle = 0
+var is_rotated = false
 
 func _ready() -> void:
 	var matrix = Basis()
@@ -14,6 +25,12 @@ func _ready() -> void:
 	matrix_top_level = tile_info["top_level"]
 	matrix_down_level = tile_info["down_level"]
 	Debug.print_debug_matrix(matrix_top_level, "Default matrix tile")
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("rotate_right") && !is_rotated:
+		rotate_clockwise()
+	if Input.is_action_just_pressed("rotate_left") && !is_rotated:
+		rotate_counterclockwise()
 
 func getTopSide() -> Array:
 	var array = [
@@ -55,6 +72,7 @@ func rotate_clockwise() -> void:
 			new_row.append(matrix_top_level[row][col])
 		rotated.append(new_row)
 	matrix_top_level = rotated
+	rotate_transform_clockwise()
 	if (Debug.ISDEBUG):
 		Debug.print_debug_matrix(matrix_top_level, "Rotate tile matrix clockwise")
 
@@ -66,5 +84,90 @@ func rotate_counterclockwise() -> void:
 			new_row.append(matrix_top_level[row][col])
 		rotated.append(new_row)
 	matrix_top_level = rotated
+	rotate_transform_counterclockwise()
 	if (Debug.ISDEBUG):
 		Debug.print_debug_matrix(matrix_top_level, "Rotate tile matrix counterclockwise")
+
+func rotate_transform_clockwise() -> void:
+	var tween = create_tween()
+	var target_rot = self.rotation_degrees + 90
+	is_rotated = true
+	
+	tween.tween_property(self, "rotation_degrees", target_rot, 0.3)
+	tween.set_ease(Tween.EASE_OUT_IN)
+	tween.play()
+	tween.finished.connect(_on_finish_rotate)
+
+func rotate_transform_counterclockwise() -> void:
+	var tween = create_tween()
+	var target_rot = self.rotation_degrees - 90
+	is_rotated = true
+	
+	tween.tween_property(self, "rotation_degrees", target_rot, 0.3)
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.play()
+	tween.finished.connect(_on_finish_rotate)
+
+func _on_finish_rotate() -> void:
+	is_rotated = false
+
+func modulate_tween_meeple(panel: Panel, color: Color) -> void:
+	var tween = create_tween()
+	
+	tween.tween_property(panel, "modulate", color, 0.2)
+	tween.set_ease(Tween.EASE_IN)
+	tween.play()
+
+func _on_panel_mouse_entered() -> void:
+	modulate_tween_meeple(panel_1, Color(1, 1, 1, 1))
+
+func _on_panel_mouse_exited() -> void:
+	modulate_tween_meeple(panel_1, Color(1, 1, 1, 0))
+
+func _on_panel_2_mouse_entered() -> void:
+	modulate_tween_meeple(panel_2, Color(1, 1, 1, 1))
+
+func _on_panel_2_mouse_exited() -> void:
+	modulate_tween_meeple(panel_2, Color(1, 1, 1, 0))
+
+func _on_panel_3_mouse_entered() -> void:
+	modulate_tween_meeple(panel_3, Color(1, 1, 1, 1))
+
+func _on_panel_3_mouse_exited() -> void:
+	modulate_tween_meeple(panel_3, Color(1, 1, 1, 0))
+
+func _on_panel_4_mouse_entered() -> void:
+	modulate_tween_meeple(panel_4, Color(1, 1, 1, 1))
+
+func _on_panel_4_mouse_exited() -> void:
+	modulate_tween_meeple(panel_4, Color(1, 1, 1, 0))
+
+func _on_panel_5_mouse_entered() -> void:
+	modulate_tween_meeple(panel_5, Color(1, 1, 1, 1))
+
+func _on_panel_5_mouse_exited() -> void:
+	modulate_tween_meeple(panel_5, Color(1, 1, 1, 0))
+
+func _on_panel_6_mouse_entered() -> void:
+	modulate_tween_meeple(panel_6, Color(1, 1, 1, 1))
+
+func _on_panel_6_mouse_exited() -> void:
+	modulate_tween_meeple(panel_6, Color(1, 1, 1, 0))
+
+func _on_panel_7_mouse_entered() -> void:
+	modulate_tween_meeple(panel_7, Color(1, 1, 1, 1))
+
+func _on_panel_7_mouse_exited() -> void:
+	modulate_tween_meeple(panel_7, Color(1, 1, 1, 0))
+
+func _on_panel_8_mouse_entered() -> void:
+	modulate_tween_meeple(panel_8, Color(1, 1, 1, 1))
+
+func _on_panel_8_mouse_exited() -> void:
+	modulate_tween_meeple(panel_8, Color(1, 1, 1, 0))
+
+func _on_panel_9_mouse_entered() -> void:
+	modulate_tween_meeple(panel_9, Color(1, 1, 1, 1))
+	
+func _on_panel_9_mouse_exited() -> void:
+	modulate_tween_meeple(panel_9, Color(1, 1, 1, 0))
